e559a2ec284192484072b91235bc92ff51a77f41
Tan Boon Xuan <boonxuan05@gmail.com>
2025-12-09 12:30:54 +0800

    Implement timer, delete and update todo

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,8 +1,45 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap"
+import { useEffect, useState, useContext } from "react"
+import { TodoContext } from "../contexts/TodoContext";
 
-export default function TodoCard({todo}) {
+export default function TodoCard({ todo }) {
     const completed = todo.completed;
-    const border = completed ? "success" : "danger";
+    const border = completed ? "sucess" : "danger";
+    const [timer, setTimer] = useState(0);
+    const [timerInterval, setTimerInterval] = useState(null);
+    const setTodos = useContext(TodoContext).setTodos;
+
+    const startTimer = () => {
+        if (timerInterval === null) {
+            const intervalID = setInterval(() => {
+                setTimer((prevTimer) => prevTimer + 1)
+            }, 1000);
+            setTimerInterval(intervalID);
+        }
+    }
+
+    const pauseTimer = () => {
+        clearInterval(timerInterval);
+        setTimerInterval(null);
+    }
+
+    const resetTimer = () => {
+        clearInterval(timerInterval);
+        setTimerInterval(null);
+        setTimer(0);
+    }
+
+    const deleteTodo = () => {
+        setTodos((prevTodos) => 
+            prevTodos.filter((prevTodo) => prevTodo.id != todo.id)
+        )
+    }
+    
+    useEffect(() => {
+        return () => {
+            clearInterval(timerInterval);
+        }
+    }, [timerInterval])
 
     return (
         <>
@@ -11,6 +48,22 @@ export default function TodoCard({todo}) {
                 <Card.Body>
                     <Card.Title>{todo.title}</Card.Title>
                     <Card.Text>{todo.description}</Card.Text>
+                    <p>Timer: {timer} seconds</p>
+                    <Button onClick={startTimer}>
+                        <i className="bi bi-play"></i>
+                    </Button>
+                    <Button onClick={pauseTimer} className="mx-2">
+                        <i className="bi bi-pause-fill"></i>
+                    </Button>
+                    <Button onClick={resetTimer} className="mx-2">
+                        <i className="bi bi-arrow-clockwise"></i>
+                    </Button>
+                    <Button variant="secondary" href={`todo/${todo.id}`} className="mx-2">
+                        <i className="bi bi-pencil"></i>
+                    </Button>
+                    <Button variant="danger" onClick={deleteTodo} className="mx-2">
+                        <i className="bi bi-trash3"></i>
+                    </Button>
                 </Card.Body>
             </Card>
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,17 +17,9 @@ export default function Home() {
 
 function CardGroup({todos}) {
     return todos.map((todo) => {
-        const completed = todo.completed;
-        const bg = completed ? "success" : "danger";
         return (
             <Col md={4} key={todo.id}>
-                <Card className="my-3">
-                    <Card.Body>
-                        <Card.Title>{todo.title}</Card.Title>
-                        <Card.Text>{todo.description}</Card.Text>
-                        <Badge bg={bg}>{!completed && "Not"} Completed</Badge>
-                    </Card.Body>
-                </Card>
+                <TodoCard todo={todo}/>
             </Col>
         )
     })
